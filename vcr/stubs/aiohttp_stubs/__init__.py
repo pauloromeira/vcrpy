f9d8c27c87e6e20d46085f1a203d7e1055e91b69
Paulo Romeira <paulo@pauloromeira.com>
2020-06-04 02:44:46 -0300

    aiohttp: Remove url field from VCR response

--- vcr/stubs/aiohttp_stubs/__init__.py
+++ vcr/stubs/aiohttp_stubs/__init__.py
@@ -62,7 +62,9 @@ def build_response(vcr_request, vcr_response, history):
         headers=CIMultiDictProxy(CIMultiDict(vcr_request.headers)),
         real_url=URL(vcr_request.url),
     )
-    response = MockClientResponse(vcr_request.method, URL(vcr_response.get("url")), request_info=request_info)
+    response = MockClientResponse(
+        vcr_request.method, request_info.url, request_info=request_info
+    )
     response.status = vcr_response["status"]["code"]
     response._body = vcr_response["body"].get("string", b"")
     response.reason = vcr_response["status"]["message"]
@@ -124,7 +126,6 @@ async def record_response(cassette, vcr_request, response):
         "status": {"code": response.status, "message": response.reason},
         "headers": _serialize_headers(response.headers),
         "body": body,  # NOQA: E999
-        "url": str(response.url),
     }
 
     cassette.append(vcr_request, vcr_response)
